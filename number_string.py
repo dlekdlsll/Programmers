e57f9d56ba6d9e52bbb250734f1b81183017a705
dlekdlsll <dtp05093@naver.com>
2021-08-11 10:46:46 +0900

    프로그래머스_숫자 문자열과 영단어

---
+++ number_string.py
@@ -0,0 +1,6 @@
+def solution(s):
+    word_dict = {'zero':'0','one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    for i in word_dict.keys(): 
+        s = s.replace(i, word_dict[i])
+    answer = int(s)
+    return answer
