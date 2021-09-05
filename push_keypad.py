d16a3c8eeb98be9f27a111826ef1c200de1338b7
dlekdlsll <dtp05093@naver.com>
2021-09-05 14:54:35 +0900

    프로그래머스_키패드 누르기(완)

--- push_keypad.py
+++ push_keypad.py
@@ -1,7 +1,12 @@
 def solution(numbers, hand):
-    import numpy as np
+    
+    def manhattan(p1, p2):
+        d = 0
+        for i in range(len(p1)):
+            d += abs(p1[i] - p2[i])
+        return d
+    
     answer = ''
-    keypad = np.array([[ (x, y) for x in (0, 1, 2) ] for y in (0, 1, 2, 3)])
     finger = {1:[0, 0], 4:[1, 0], 7:[2, 0], '*':[3, 0], 2:[0, 1],5:[1, 1],8:[2, 1],0:[3, 1],3:[0, 2],6:[1, 2],9:[2, 2],'#':[3, 2]}
     L_finger = finger['*']; R_finger = finger['#']
     
@@ -12,18 +17,18 @@ def solution(numbers, hand):
         elif i in (3, 6, 9):
             R_finger = finger[i]
             answer += 'R'
-        elif i in (2, 5, 8, 0):
-            if abs(sum(np.array(L_finger))) == abs(sum(np.array(R_finger))):
-                if hand == 'right':
-                    answer += 'R'
-                    R_finger = finger[i]
-                else:
-                    answer += 'L'
+        else:
+            if manhattan(finger[i], L_finger) == manhattan(finger[i], R_finger):
+                if hand == 'left':
                     L_finger = finger[i]
-            elif abs(sum(np.array(L_finger))) > abs(sum(np.array(L_finger))):
-                answer += 'R'
+                    answer += 'L'
+                else:
+                    R_finger = finger[i]
+                    answer += 'R'
+            elif manhattan(finger[i], L_finger) > manhattan(finger[i], R_finger):
                 R_finger = finger[i]
-            elif abs(sum(np.array(L_finger))) < abs(sum(np.array(L_finger))):
-                answer += 'L'
+                answer += 'R'
+            else:
                 L_finger = finger[i]
+                answer += 'L'
     return answer
