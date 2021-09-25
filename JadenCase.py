7a47dbbc8ad1a522d7b15910f1be1213ed778340
dlekdlsll <dtp05093@naver.com>
2021-09-25 12:52:14 +0900

    프로그래머스_JadenCase 문자열 만들기

--- JadenCase.py
+++ JadenCase.py
@@ -1,2 +1,2 @@
 def solution(s):
-    return ''.join([i.upper() if i.isdigit() == False and (s.find(i) == 0 or s[s.find(i, j)-1] == ' ') else i.lower() for j,i in enumerate(s)])
+    return ''.join([i.upper() if i.isdigit() == False and (s.find(i, j) == 0 or s[s.find(i, j)-1] == ' ') else i.lower() for j,i in enumerate(s)])
