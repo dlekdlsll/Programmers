6b71d79420a87b3607bf46f29bb2a5b07323f49c
dlekdlsll <dtp05093@naver.com>
2021-09-26 11:53:40 +0900

    프로그래머스_짝지어 제거하기(미완료)

--- couple_delete.py
+++ couple_delete.py
@@ -1,10 +1,9 @@
 def solution(s):
-    result = list(s)
-    for i in range(0, len(result[1:])):
-        try:
-            if result[i] == result[i-1]:
-                result.remove(result[i-1:i+1])
-        except:
-            result = ''
-    answer = 1 if len(result) == 0 else 0
+    alpha = ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 
+             'jj', 'kk', 'll', 'mm', 'nn', 'oo', 'pp', 'qq', 'rr', 
+             'ss', 'tt', 'uu','vv', 'ww', 'xx', 'yy', 'zz']
+    for j in range(0, 10):
+        for i in alpha:
+            s = s.replace(i, '')
+    answer = 1 if len(s) == 0 else 0
     return answer
