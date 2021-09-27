583c22c4279f0949ced96139ac1ba35a21b20adc
dlekdlsll <dtp05093@naver.com>
2021-09-27 22:49:04 +0900

    프로그래머스_전화번호 목록(미완료)

---
+++ phone_book.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    val = set(phone_book)
+    phone_book = ','.join(val)
+    count = 0
+    for i in val:
+        location = phone_book.find(i)+1
+        if phone_book.find(i, location) != -1:
+            count += 1
+    
+    answer = False if count > 0 else True
+    return answer
