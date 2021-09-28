9072908d176392410f719c008f8a86c485a8defd
dlekdlsll <dtp05093@naver.com>
2021-09-28 15:23:47 +0900

    프로그래머스_전화번호 목록(미완료)

--- phone_book.py
+++ phone_book.py
@@ -1,11 +1,10 @@
 def solution(phone_book):
-    val = set(phone_book)
+    val = list(set(phone_book))
     phone_book = ','.join(val)
     count = 0
     for i in val:
-        location = phone_book.find(i)+1
-        if phone_book.find(i, location) != -1:
-            count += 1
+        location = (phone_book.find(i))+1
+        count += 1 if phone_book.find(i, location) != -1 else 0
     
     answer = False if count > 0 else True
     return answer
