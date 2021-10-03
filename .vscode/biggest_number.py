af96e9f38f5b8b2a87675fa25cab3beaa1982d28
dlekdlsll <dtp05093@naver.com>
2021-10-03 15:06:44 +0900

    프로그래머스_가장 큰 수

--- .vscode/biggest_number.py
+++ .vscode/biggest_number.py
@@ -1,2 +1,4 @@
 def solution(numbers):
-    return ''.join(sorted(map(str,numbers),key = lambda x: (x*3)[-1], reverse = True))
+    answer = ''.join(sorted(map(str,numbers),key = lambda x: x*3, reverse = True))
+    if answer.replace('0', '') == '': answer = '0'
+    return answer
