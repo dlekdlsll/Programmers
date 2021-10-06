25ba297e3b66970c80911e05b835f45f0fc142dd
dlekdlsll <dtp05093@naver.com>
2021-10-06 09:29:31 +0900

    프로그래머스_땅따먹기(미완료)

---
+++ .vscode/eat_the_ground.py
@@ -0,0 +1,8 @@
+def solution(land):
+    answer = max(land[0])
+    loc = land[0].index(max(land[0]))
+    for i in land[1:]:
+        res = max([j for j in i if i.index(j)!=loc])
+        loc = i.index(res)
+        answer += res
+    return answer
