f377dc8a38aba70c499990cf5d7a424db1be506e
dlekdlsll <dtp05093@naver.com>
2021-10-21 22:12:18 +0900

    프로그래머스_올바른 괄호(미완료)

--- parenthesis.py
+++ parenthesis.py
@@ -1,8 +1,11 @@
 def solution(s):
-    if s[0] == ')':
-        answer = False
-    elif s[-1] == '(':
-        answer = False
+    if s.count('(') == s.count(')'):
+        if s[0] == ')':
+            answer = False
+        elif s[-1] == '(':
+            answer = False
+        else:
+            answer = True
     else:
-        answer = True
+        answer = False
     return answer
