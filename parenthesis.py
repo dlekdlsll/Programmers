61e1817725d35309c7e6767654618a52647eae54
dlekdlsll <dtp05093@naver.com>
2021-10-27 23:18:15 +0900

    프로그래머스_올바른 괄호(미완료)

--- parenthesis.py
+++ parenthesis.py
@@ -1,12 +1,11 @@
 def solution(s):
     flag = 0
     if s.count('(') == s.count(')'):
+        flag = 1
         if s[0] == ')':
             flag = 1
         elif s[-1] == '(':
             flag = 0
-        else:
-            flag = 1
     else:
         flag = 0
     answer = True if flag == 1 else False
