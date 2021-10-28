ebc7d0a140076936fc42b42d32793071956dc0f3
dlekdlsll <dtp05093@naver.com>
2021-10-29 00:01:53 +0900

    프로그래머스_올바른 괄호(미완료)

--- parenthesis.py
+++ parenthesis.py
@@ -6,6 +6,8 @@ def solution(s):
             flag = 2
         elif s[-1] == '(':
             flag = 1
+            if s[-2] == '(':
+                flag = 1
     else:
         flag = 0
     answer = True if flag == 1 else False
